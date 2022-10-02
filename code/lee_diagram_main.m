f5bffaeb6ff2acbf7ebe4ce8faa9d32013ebb794
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-10-03 00:52:30 +0800

    update lee diagram

--- code/lee_diagram_main.m
+++ code/lee_diagram_main.m
@@ -1,53 +1,59 @@
 clear; close all; clc;
 
-dq = 0.01;
-theta = (137:dq:145)';
-a_store = 10.^(1:.002:3);     % drop radii, in um
+dq = 0.02;
+theta_lim = [137, 145];
+theta = (theta_lim(1)-0.5:dq:theta_lim(2)+0.5)';
+a_store = 10.^(3:-.01:1);     % drop radii, in um
 
 lee_diagram = zeros(length(theta), length(a_store), 3);
 for ai = 1:length(a_store)
     a = a_store(ai);
     fprintf('Computing a: %.2f, #%d/%d\n', a, ai, length(a_store));
-    if a < 200
-        lambda_num = floor(6000 / a);
-    else
-        lambda_num = 30;
-    end
+
+    lambda_num = max(floor(6000 / a), 30);
     lambda = linspace(0.42, 0.68, lambda_num);   % in um
+    dw = lambda(2) - lambda(1);
     sun_spec = colorvis.black_body_radiance(lambda * 1000, 5700);
-    sun_spec = sun_spec / sum(sun_spec);
+    sun_spec = sun_spec / sum(sun_spec * dw);
 
     intensity = water_drop_scattering(a, lambda, theta, 'SunSize', 0.5);
-%     intensity = intensity ./ max(intensity(:));
+    % energy of each wavelength sums up to that portion of sun light
+    intensity = intensity ./ sum(intensity * dw * dq) .* sun_spec;
+    intensity = intensity ./ prctile(intensity(:), 95) * 5e-3;
 
-    colors = spec_to_rgb([lambda(:) * 1000, intensity' .* sun_spec' * 2 * lambda_num], 'Y', -1);
+    colors = spec_to_rgb([lambda(:) * 1000, intensity'], 'Y', -1);
     lee_diagram(:, ai, :) = reshape(colors, [], 1, 3);
 
     figure(1); clf;
     set(gcf, 'Position', [500, 20, 500, 800]);
-    set(gca, 'Position', [.1, .05, .8, .9]);
+    axes('Position', [.13, .08, .84, .9]);
     imagesc(log10(a_store), theta, lee_diagram);
-
-    xtick = [1, 2, 3];
-    xtick_label = cell(length(xtick), 1);
-    for i = 1:length(xtick)
-        xtick_label{i} = sprintf('%.1f', 10^xtick(i));
-    end
-    set(gca, 'xticklabel', xtick_label, 'xtick', xtick, 'ydir', 'reverse');
+    set(gca, 'ydir', 'reverse', 'ylim', theta_lim);
+    axis off;
+    axes('Position', [.13, .08, .84, .9], 'Color', 'none');
+    set(gca, 'ydir', 'reverse', 'ylim', theta_lim, ...
+        'xlim', [10, 1000]/1000, 'xscale', 'log', 'tickdir', 'out', ...
+        'fontsize', 12);
+    box on;
     drawnow;
 end
 
 %%
 figure(1); clf;
 set(gcf, 'Position', [500, 20, 500, 800]);
-set(gca, 'Position', [.1, .05, .8, .9]);
+axes('Position', [.13, .08, .84, .9]);
 imagesc(log10(a_store), theta, lee_diagram);
-
-xtick = [1, 2, 3];
-xtick_label = cell(length(xtick), 1);
-for i = 1:length(xtick)
-    xtick_label{i} = sprintf('%.1f', 10^xtick(i));
-end
-set(gca, 'xticklabel', xtick_label, 'xtick', xtick, 'ydir', 'reverse');
-axis ij;
+set(gca, 'ydir', 'reverse', 'ylim', theta_lim);
+axis off;
+axes('Position', [.13, .08, .84, .9], 'Color', 'none');
+set(gca, 'ydir', 'reverse', 'ylim', theta_lim, ...
+    'xlim', [10, 1000]/1000, 'xscale', 'log', 'tickdir', 'out', ...
+    'fontsize', 12);
+box on;
+xlabel('Radius (mm)', 'fontsize', 16);
+ylabel('Scattering angle (degree)', 'fontsize', 16);
 drawnow;
+
+postfix = sprintf('a%04d-%04d_q%03d-%03d', min(a_store), max(a_store), theta_lim(1), theta_lim(2));
+saveas(gcf, sprintf('../out/lee_diagram_%s.png', postfix));
+imwrite(uint8(lee_diagram * 256), sprintf('../out/lee_diagram_data_%s.png', postfix));
