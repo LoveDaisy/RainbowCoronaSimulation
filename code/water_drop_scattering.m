257a683965f2eca8cf98a6b3773c95becf0114e3
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-10-02 12:45:55 +0800

    improve performance

--- code/water_drop_scattering.m
+++ code/water_drop_scattering.m
@@ -13,7 +13,6 @@ if p.Results.SunSize > 0
     r = p.Results.SunSize / 2;
     smoothing_kernel = sqrt(1 - ((-r:dq:r) / r).^2);
     smoothing_kernel = smoothing_kernel / sum(smoothing_kernel);
-    pad_size = ceil(length(smoothing_kernel) / 2);
 end
 
 intensity = zeros(length(theta), length(lambda));
@@ -21,11 +20,9 @@ for i = 1:length(lambda)
     fprintf('Computing lambda %.3fum, #%d/%d\n', lambda(i), i, length(lambda));
     curr_intensity = mie_theory_scattering(a, m(i), lambda(i), theta);
     curr_intensity = curr_intensity / sum(curr_intensity);
-    if p.Results.SunSize > 0
-        curr_intensity = padarray(curr_intensity, [1, 0] * pad_size, 'both', 'replicate');
-        curr_intensity = conv(curr_intensity, smoothing_kernel, 'same');
-        curr_intensity = curr_intensity(pad_size+1:end-pad_size);
-    end
     intensity(:, i) = curr_intensity;
+end
+if p.Results.SunSize > 0
+    intensity = imfilter(intensity, smoothing_kernel(:), 'same', 'replicate');
 end
 end
