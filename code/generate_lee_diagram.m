1dc552f89bb845192e2f218c1b86bf5b9ec479be
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-10-04 23:21:37 +0800

    extract lee diagram function

---
+++ code/generate_lee_diagram.m
@@ -0,0 +1,54 @@
+function lee_diagram_data = generate_lee_diagram(drop_radius, theta, varargin)
+p = inputParser;
+p.addRequired('drop_radius', @(x) isnumeric(x) && isvector(x));
+p.addRequired('theta', @(x) isnumeric(x) && isvector(x));
+p.addParameter('SunSize', 0.5, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('WSampleFactor', 1, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('IntensityFactor', [95, 1], @(x) isnumeric(x) && isvector(x) && length(x) == 2);
+p.addParameter('IterateDir', 1, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('Debug', false, @(x) islogical(x) && isscalar(x));
+p.parse(drop_radius, theta, varargin{:});
+
+dq = abs(theta(2) - theta(1));
+theta_lim = [min(theta), max(theta)];
+a_num = length(drop_radius);
+lee_diagram_data = zeros(length(theta), a_num, 3);
+if p.Results.IterateDir > 1
+    ai_store = 1:a_num;
+else
+    ai_store = a_num:-1:1;
+end
+for ai = ai_store
+    a = drop_radius(ai);
+    fprintf('Computing a: %.2f, #%d/%d\n', a, ai, a_num);
+
+    lambda_num = max(floor(5000 * p.Results.WSampleFactor / a), floor(20 * p.Results.WSampleFactor));
+    lambda = linspace(0.42, 0.68, lambda_num);   % in um
+    dw = lambda(2) - lambda(1);
+    sun_spec = colorvis.black_body_radiance(lambda * 1000, 5700);
+    sun_spec = sun_spec / sum(sun_spec * dw);
+
+    intensity = water_drop_scattering(a, lambda, theta, 'SunSize', p.Results.SunSize, 'Parallel', true);
+    % energy of each wavelength sums up to that portion of sun light
+    intensity = intensity ./ sum(intensity * dw * dq) .* sun_spec;
+    intensity = intensity ./ prctile(intensity(:), p.Results.IntensityFactor(1)) * 5e-3 * p.Results.IntensityFactor(2);
+
+    colors = spec_to_rgb([lambda(:) * 1000, intensity'], 'Y', -1);
+    lee_diagram_data(:, ai, :) = reshape(colors, [], 1, 3);
+
+    if p.Results.Debug
+        axes('Position', [.13, .08, .84, .9]);
+        imagesc(log10(drop_radius), theta, lee_diagram_data);
+        set(gca, 'ydir', 'reverse', 'ylim', theta_lim);
+        axis off;
+        axes('Position', [.13, .08, .84, .9], 'Color', 'none');
+        set(gca, 'ydir', 'reverse', 'ylim', theta_lim, ...
+            'xlim', [min(drop_radius), max(drop_radius)]/1000, 'xscale', 'log', 'tickdir', 'out', ...
+            'fontsize', 12);
+        box on;
+        xlabel('Radius (mm)', 'fontsize', 16);
+        ylabel('Scattering angle (degree)', 'fontsize', 16);
+        drawnow;
+    end
+end
+end
--- code/lee_diagram_main.m
+++ code/lee_diagram_main.m
@@ -1,42 +1,14 @@
 clear; close all; clc;
 
-dq = 0.01;
-theta_lim = [137, 145];
+dq = 0.02;
+theta_lim = [170, 180];
 theta = (theta_lim(1)-0.5:dq:theta_lim(2)+0.5)';
-a_store = 10.^(1:.002:3);     % drop radii, in um
+a_store = 10.^(0:.002:log10(21));     % drop radii, in um
 
-lee_diagram = zeros(length(theta), length(a_store), 3);
-for ai = length(a_store):-1:1
-    a = a_store(ai);
-    fprintf('Computing a: %.2f, #%d/%d\n', a, ai, length(a_store));
+figure(1); clf;
+set(gcf, 'Position', [500, 20, 500, 800]);
 
-    lambda_num = max(floor(100000 / a), 500);
-    lambda = linspace(0.42, 0.68, lambda_num);   % in um
-    dw = lambda(2) - lambda(1);
-    sun_spec = colorvis.black_body_radiance(lambda * 1000, 5700);
-    sun_spec = sun_spec / sum(sun_spec * dw);
-
-    intensity = water_drop_scattering(a, lambda, theta, 'SunSize', 0.5, 'Parallel', true);
-    % energy of each wavelength sums up to that portion of sun light
-    intensity = intensity ./ sum(intensity * dw * dq) .* sun_spec;
-    intensity = intensity ./ prctile(intensity(:), 95) * 5e-3;
-
-    colors = spec_to_rgb([lambda(:) * 1000, intensity'], 'Y', -1);
-    lee_diagram(:, ai, :) = reshape(colors, [], 1, 3);
-
-    figure(1); clf;
-    set(gcf, 'Position', [500, 20, 500, 800]);
-    axes('Position', [.13, .08, .84, .9]);
-    imagesc(log10(a_store), theta, lee_diagram);
-    set(gca, 'ydir', 'reverse', 'ylim', theta_lim);
-    axis off;
-    axes('Position', [.13, .08, .84, .9], 'Color', 'none');
-    set(gca, 'ydir', 'reverse', 'ylim', theta_lim, ...
-        'xlim', [min(a_store), max(a_store)]/1000, 'xscale', 'log', 'tickdir', 'out', ...
-        'fontsize', 12);
-    box on;
-    drawnow;
-end
+lee_diagram = generate_lee_diagram(a_store, theta, 'WSampleFactor', 10, 'Debug', true);
 
 %%
 figure(1); clf;
