72396c075bbeccbd894f147a78495a5c992cd84d
Jiajie Zhang <zhangjiajie043@gmail.com>
2022-10-04 10:50:38 +0800

    update test scripts

---
+++ code/check_single_lambda.m
@@ -0,0 +1,17 @@
+clear; close all; clc;
+
+a = 100;
+
+lambda = 0.65;
+dq = 0.01;
+m = 1.332;
+theta = 137:dq:145;
+
+tic;
+intensity = mie_theory_scattering(a, m, lambda, theta);
+toc;
+
+%%
+figure(1); clf;
+plot(theta, intensity);
+set(gca, 'yscale', 'linear');
